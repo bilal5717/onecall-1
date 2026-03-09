3cafe8d31cbdf6f83c71b1b5aa46269453832e98
Muhammad Bilal <145279530+bilal5717@users.noreply.github.com>
2026-03-09 15:54:57 +0500

    vercel deploy issue fixed

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            buildDirectory: 'dist',
             refresh: true,
         }),
     ],
