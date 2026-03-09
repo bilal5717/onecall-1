f0707e7833d57942a68a12d00153d442a7640f15
Muhammad Bilal <145279530+bilal5717@users.noreply.github.com>
2026-03-09 16:04:01 +0500

    issue for deploy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,10 +9,11 @@ class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
+     * Use '*' when behind Vercel or other reverse proxies.
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
